6fbbb19d8464d1089f7715bc25b114698c64c502
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-04-12 11:09:04 +0200

    Done

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
-const mongoose = require("mongoose");
-const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
-const data = require("./data.js"); // Import of the data from './data.js'
+const mongoose = require('mongoose');
+const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect("mongodb://localhost/recipeApp", { useNewUrlParser: true });
-// .then(() => {
-//   console.log("Connected to Mongo!");
-// })
-// .catch(err => {
-//   console.error("Error connecting to mongo", err);
-// });
+mongoose
+  .connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to Mongo!');
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err);
+  });
 
-// Iteration 2
-// Recipe.create({
-//   title: "Tacos",
-//   level: "Amateur Chef",
-//   ingredients: ["Corn", "Salsa", "...etc"],
-//   cuisine: "Whatever",
-//   dishType: "Dish",
-//   duration: 120,
-//   creator: "Me"
-// })
-//   .then(recipe => {
-//     console.log(recipe.title);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
+const createRecipe = () =>
+  Recipe.create({
+    title: 'Tacos',
+    level: 'Easy Peasy',
+    ingredients: ['beans', 'avocado', 'tortilla'],
+    cuisine: 'Tex-Mex',
+    dishType: 'Dish',
+    image:
+      'https://igav3-metcdn-com.global.ssl.fastly.net/content/uploads/sites/2/2017/06/05162419/beef-tacos.jpg',
+    duration: 5,
+    creator: 'Juan'
+  });
 
-// Iteration 3
-// Recipe.insertMany(data)
-//   .then(recipes =>
-//     recipes.forEach(recipe => {
-//       console.log(recipe.title);
-//     })
-//   )
-//   .catch(err => {
-//     console.error(err);
-//   });
+const insertRecipes = () =>
+  Recipe.insertMany(data).then(docs => {
+    docs.forEach(el => {
+      console.log(el.title);
+    });
+  });
 
-// Iteration 4
-// Recipe.findByIdAndUpdate("5caf9f84f074e55b21236bbb", { duration: 100 })
-// Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
-//   .then(update => {
-//     console.log("Update: success!");
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
+const updateRigatoniRecipe = () =>
+  Recipe.findOneAndUpdate(
+    { title: 'Rigatoni alla Genovese' },
+    { duration: 100 }
+  ).then(data => {
+    console.log('Success!');
+  });
 
-// Iteration 5
-// Recipe.findByIdAndRemove("5caf97eef46aab578d24484b")
-// Recipe.deleteOne({ title: "Carrot Cake" })
-//   .then(remove => {
-//     console.log("Delete: success!");
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
+const deleteCarrotCake = () =>
+  Recipe.deleteOne({ title: 'Carrot Cake' }).then(data => {
+    console.log('Success!');
+  });
 
-// Iteration 6
+createRecipe()
+  .then(insertRecipes)
+  .then(updateRigatoniRecipe)
+  .then(deleteCarrotCake)
+  .then(() => {
+    console.log('disconnected');
+    mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(err);
+  });
